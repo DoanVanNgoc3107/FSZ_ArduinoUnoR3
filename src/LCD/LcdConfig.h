34f4b8580592625fa91c4ed0353c177db0e434bc
Doan Van Ngoc <doanvanngoc3107@gmail.com>
2026-01-01 23:58:17 +0700

    Add new button and sensor configuration files for logic service

---
+++ src/LCD/LcdConfig.h
@@ -0,0 +1,13 @@
+//
+// Created by ngoc1 on 1/1/2026.
+//
+
+#ifndef FSZ_ARDUINOUNOR3_LCDCONFIG_H
+#define FSZ_ARDUINOUNOR3_LCDCONFIG_H
+
+
+class LcdConfig {
+};
+
+
+#endif //FSZ_ARDUINOUNOR3_LCDCONFIG_H
